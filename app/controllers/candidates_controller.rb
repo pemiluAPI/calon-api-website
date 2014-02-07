a777488be58145cb78f0d4b32a3075f7ba675ea2
Erie <erie.suharko@wgs.co.id>
2014-02-07 11:18:52 +0700

    update view of list and detail calon

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,10 +1,10 @@
 class CandidatesController < ApplicationController
   def index
-    @response = HTTParty.get("#{Rails.configuration.pemilu_api_endpoint}/api/candidates?apiKey=#{Rails.configuration.pemilu_api_key}")
+    @response = HTTParty.get("#{Rails.configuration.pemilu_api_endpoint}/api/caleg?apiKey=#{Rails.configuration.pemilu_api_key}")    
   end
 
   def show
-    response = HTTParty.get("#{Rails.configuration.pemilu_api_endpoint}/api/candidates/#{params[:id]}?apiKey=#{Rails.configuration.pemilu_api_key}")
-    @candidate = response.parsed_response['data']['results']['candidates'].first
+    response = HTTParty.get("#{Rails.configuration.pemilu_api_endpoint}/api/caleg/#{params[:id]}?apiKey=#{Rails.configuration.pemilu_api_key}")
+    @caleg = response.parsed_response['data']['results']['caleg'].first
   end
 end
